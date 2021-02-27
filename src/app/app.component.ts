398cc74552f90aacf691eb9f57144878cf6ecde6
Vidya Muniraju <vidya.muniraju@csu.fullerton.edu>
2021-02-26 19:51:05 -0600

    Added user registration and login components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+
+  // this is the fn that will open the dialog when the signup button is clciked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // assigning the dialog a width
+      width: '280px'
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
+
 }
--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 // Decalring the api url that will provide data for the client app
-const apiUrl = 'https://flix-world.herokuapp.com';
+const apiUrl = 'https://flix-world.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
